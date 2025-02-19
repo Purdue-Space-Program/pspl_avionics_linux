9a011f15127edd6b44d22121bca8b57400e9c930
Sagar Patil <sagarreddypatil@gmail.com>
2025-02-19 14:06:39 -0800

    separate var log partition, read-only squashfs root

---
+++ buildroot-external/board/raspberrypi/post-image.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+BOARD_DIR="$(dirname $0)"
+GENIMAGE_PATH="${BOARD_DIR}/genimage.cfg"
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+
+cp "${GENIMAGE_PATH}" "${BINARIES_DIR}/genimage.cfg"
+
+# idk man im just copy pasting from buildroot/board/raspberrypi4-64/post-image.sh
+trap 'rm -rf "${ROOTPATH_TMP}"' EXIT
+ROOTPATH_TMP="$(mktemp -d)"
+
+rm -rf "${GENIMAGE_TMP}"
+
+genimage \
+    --rootpath   "${ROOTPATH_TMP}"   \
+    --tmppath    "${GENIMAGE_TMP}"   \
+    --inputpath  "${BINARIES_DIR}"   \
+    --outputpath "${BINARIES_DIR}"   \
+    --config     "${BINARIES_DIR}/genimage.cfg"
+
+exit $?

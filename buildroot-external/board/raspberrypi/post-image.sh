637898e029accbdfab9657efb125b4b1b22dfc9b
Sagar Patil <sagarreddypatil@gmail.com>
2025-02-27 21:40:01 -0800

    fix ssh

--- buildroot-external/board/raspberrypi/post-image.sh
+++ buildroot-external/board/raspberrypi/post-image.sh
@@ -8,6 +8,11 @@ GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
 cp "${GENIMAGE_PATH}" "${BINARIES_DIR}/genimage.cfg"
 
+mkdir -p -p "${BINARIES_DIR}/ssh"
+ssh-keygen -q -N "" -t rsa -b 4096 -f "${BINARIES_DIR}/ssh/ssh_host_rsa_key"
+ssh-keygen -q -N "" -t ecdsa -f "${BINARIES_DIR}/ssh/ssh_host_ecdsa_key"
+ssh-keygen -q -N "" -t ed25519 -f "${BINARIES_DIR}/ssh/ssh_host_ed25519_key"
+
 # idk man im just copy pasting from buildroot/board/raspberrypi4-64/post-image.sh
 trap 'rm -rf "${ROOTPATH_TMP}"' EXIT
 ROOTPATH_TMP="$(mktemp -d)"

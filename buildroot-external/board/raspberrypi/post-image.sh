3336276ba2535f452f52f7eec5be5a23e19de9cf
Sagar Patil <sagarreddypatil@gmail.com>
2025-02-27 21:45:34 -0800

    remove empty=true flag for var lib

--- buildroot-external/board/raspberrypi/post-image.sh
+++ buildroot-external/board/raspberrypi/post-image.sh
@@ -8,16 +8,17 @@ GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
 cp "${GENIMAGE_PATH}" "${BINARIES_DIR}/genimage.cfg"
 
-mkdir -p -p "${BINARIES_DIR}/ssh"
-ssh-keygen -q -N "" -t rsa -b 4096 -f "${BINARIES_DIR}/ssh/ssh_host_rsa_key"
-ssh-keygen -q -N "" -t ecdsa -f "${BINARIES_DIR}/ssh/ssh_host_ecdsa_key"
-ssh-keygen -q -N "" -t ed25519 -f "${BINARIES_DIR}/ssh/ssh_host_ed25519_key"
-
 # idk man im just copy pasting from buildroot/board/raspberrypi4-64/post-image.sh
 trap 'rm -rf "${ROOTPATH_TMP}"' EXIT
 ROOTPATH_TMP="$(mktemp -d)"
 
 rm -rf "${GENIMAGE_TMP}"
+
+mkdir -p "${ROOTPATH_TMP}/var/lib/ssh"
+ssh-keygen -q -N "" -t rsa -b 4096 -f "${ROOTPATH_TMP}/var/lib/ssh/ssh_host_rsa_key"
+ssh-keygen -q -N "" -t ecdsa -f       "${ROOTPATH_TMP}/var/lib/ssh/ssh_host_ecdsa_key"
+ssh-keygen -q -N "" -t ed25519 -f     "${ROOTPATH_TMP}/var/lib/ssh/ssh_host_ed25519_key"
+
 
 genimage \
     --rootpath   "${ROOTPATH_TMP}"   \

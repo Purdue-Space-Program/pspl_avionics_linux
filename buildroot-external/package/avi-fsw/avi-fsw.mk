bbe5ecbb83a02e85e6792c1aac3117bb86d995de
Sagar Patil <sagarreddypatil@gmail.com>
2025-02-27 22:34:20 -0800

    nuke daqhats

--- buildroot-external/package/avi-fsw/avi-fsw.mk
+++ buildroot-external/package/avi-fsw/avi-fsw.mk
@@ -1,6 +1,6 @@
 AVI_FSW_VERSION = 0.1.0
 AVI_FSW_SITE = $(call github,Purdue-Space-Program,PSPL_CMS_AVIONICS_COTS_FSW,main)
 AVI_FSW_LICENSE = PROPRIETARY
-AVI_FSW_DEPENDENCIES = libgpiod daqhats
+AVI_FSW_DEPENDENCIES = libgpiod
 
 $(eval $(cmake-package))

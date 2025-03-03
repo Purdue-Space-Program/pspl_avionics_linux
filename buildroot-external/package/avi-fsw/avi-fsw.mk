cf62b26cc8a23e7a431524314d0cc1bc310d2070
Sagar Patil <sagarreddypatil@gmail.com>
2025-03-02 21:50:46 -0800

    avi fsw makefile

--- buildroot-external/package/avi-fsw/avi-fsw.mk
+++ buildroot-external/package/avi-fsw/avi-fsw.mk
@@ -3,4 +3,15 @@ AVI_FSW_SITE = $(call github,Purdue-Space-Program,PSPL_CMS_AVIONICS_COTS_FSW,mai
 AVI_FSW_LICENSE = PROPRIETARY
 AVI_FSW_DEPENDENCIES = libgpiod
 
-$(eval $(cmake-package))
+AVI_FSW_CFLAGS = $(TARGET_CFLAGS) -Iinclude/ -std=c11
+AVI_FSW_CXXFLAGS = $(TARGET_CXXFLAGS) -Iinclude/ -std=c++20
+
+define AVI_FSW_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) CFLAGS="$(AVI_FSW_CFLAGS)" CXXFLAGS="$(AVI_FSW_CXXFLAGS)" -C $(@D) all
+endef
+
+define AVI_FSW_INSTALL_TARGET_CMDS
+	$(MAKE) -C $(@D) install PREFIX=$(TARGET_DIR)
+endef
+
+$(eval $(generic-package))
